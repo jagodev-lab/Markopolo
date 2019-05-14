86b7148f94437e394b293a45d921fade4b75bf11
Andrea Dejan Grande <dejan-grande@mail.com>
2019-05-14 23:59:44 +0200

    Added debugging to database clearing process

--- scripts/clear.js
+++ scripts/clear.js
@@ -4,6 +4,9 @@ const client = new Client({ username: 'testuser', password: 'testpassword', port
 const MongoClient = require('mongodb').MongoClient;
 const mongoUrl = "mongodb://localhost:27017/";
 
+// Cyan background color
+console.log("\x1b[44m%s\x1b[0m%s", "EVENT:", " database cleaning started!");
+
 MongoClient.connect(mongoUrl, { useNewUrlParser: true }, function(err, db) {
   if (err) {
     throw err;
@@ -17,6 +20,9 @@ MongoClient.connect(mongoUrl, { useNewUrlParser: true }, function(err, db) {
         throw err;
       }
 
+      // Cyan background color
+      console.log("\x1b[44m%s\x1b[0m%s", "EVENT:", " \"info\" collection dropped!");
+
       // Delete current "addresses" collection
       dbo.collection("addresses").drop(
         function(err, res) {
@@ -24,12 +30,20 @@ MongoClient.connect(mongoUrl, { useNewUrlParser: true }, function(err, db) {
             throw err;
           }
 
+          // Cyan background color
+          console.log("\x1b[44m%s\x1b[0m%s", "EVENT:", " \"addresses\" collection dropped!");
+
           // Delete current "transactions" collection
           dbo.collection("transactions").drop(
             function(err, res) {
               if (err) {
                 throw err;
               }
+
+              // Cyan background color
+              console.log("\x1b[44m%s\x1b[0m%s", "EVENT:", " \"transactions\" collection dropped!");
+              // Green background color
+              console.log("\x1b[42m%s\x1b[0m%s", "SUCCESS:", " blockchain cleaning completed!");
 
               db.close();
             }
